45a337ffa6a3097785edf683b7eb002f9cdc757f
Konstantin Prokopchik <konstantin.prokopchik@andriyanov.com>
2026-08-03 20:54:01 +0200

    finished exercise 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,85 +1,103 @@
 import { useState } from 'react'
+import { useEffect } from 'react'
+
+import axios from 'axios'
 
 const Filter = ({ filter, setNewFilter }) => {
-  return (
-    <form>
-      <div>
-        filter shown with <input value={filter} onChange={(event) => { setNewFilter(event.target.value) }} />
-      </div>
-    </form>
-  )
+    return (
+        <form>
+            <div>
+                filter shown with <input value={filter} onChange={(event) => { setNewFilter(event.target.value) }} />
+            </div>
+        </form>
+    )
 
 }
+
+
+
 
 const NewPersonForm = ({ addToPhoneBook, newName, newPhoneNumber, setNewName, setNewPhoneNumber }) => {
-  return (
-    <form onSubmit={addToPhoneBook}>
-      <div>
-        name:
-        <input value={newName} onChange={(event) => { setNewName(event.target.value) }} />
-      </div>
-      <div>
-        number:
-        <input value={newPhoneNumber} onChange={(event) => { setNewPhoneNumber(event.target.value) }} />
-      </div>
-      <div>
-        <button type="submit" >add</button>
-      </div>
-    </form>
-  )
+    return (
+        <form onSubmit={addToPhoneBook}>
+            <div>
+                name:
+                <input value={newName} onChange={(event) => { setNewName(event.target.value) }} />
+            </div>
+            <div>
+                number:
+                <input value={newPhoneNumber} onChange={(event) => { setNewPhoneNumber(event.target.value) }} />
+            </div>
+            <div>
+                <button type="submit" >add</button>
+            </div>
+        </form>
+
+    )
 }
 
+
 const Persons = ({ filterPersons }) => {
-  return (
-    <ol>
-      {filterPersons.map(person => <Person key={person.id} person={person} />)}
-    </ol>
-  )
+    return (
+        <ol>
+            {filterPersons.map(person => <Person key={person.id} person={person} />)}
+        </ol>
+    )
 
 }
 
 const Person = ({ person }) => {
-  return (<li>{person.name} {person.phoneNumber}</li>)
+    return (<li>{person.name} {person.number}</li>)
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', phoneNumber: '39-44-5556', id: 1 }
-  ])
-  const [newName, setNewName] = useState('')
-  const [newPhoneNumber, setNewPhoneNumber] = useState('')
-  const [filter, setNewFilter] = useState('')
 
-  const addToPhoneBook = (event) => {
-    event.preventDefault()
-    if (persons.map(person => person.name).includes(newName)) {
-      window.alert(`This name (${newName}) has already been added to the Phone Book`)
+
+    const [newName, setNewName] = useState('')
+    const [persons, setPersons] = useState([])
+    const [newPhoneNumber, setNewPhoneNumber] = useState('')
+    const [filter, setNewFilter] = useState('')
+    useEffect(() => {
+        console.log('effect')
+        axios
+            .get('http://localhost:3001/persons')
+            .then(response => {
+                setPersons(response.data)
+            })
+    }, [])
+
+
+    const addToPhoneBook = (event) => {
+        event.preventDefault()
+        if (persons.map(person => person.name).includes(newName)) {
+            window.alert(`This name (${newName}) has already been added to the Phone Book`)
+        }
+        const newPerson = {
+            name: newName,
+            number: newPhoneNumber,
+            id: persons.length + 1
+        }
+        setPersons(persons.concat(newPerson))
+        setNewName('')
+        setNewPhoneNumber('')
+
     }
-    const newPerson = {
-      name: newName,
-      phoneNumber: newPhoneNumber,
-      id: persons.length + 1
-    }
-    setPersons(persons.concat(newPerson))
-    setNewName('')
-    setNewPhoneNumber('')
-
-  }
-
-  const filterPersons = persons.filter(persons => persons.name.toLowerCase().startsWith(filter))
 
 
+    console.log(persons)
+    const filterPersons = persons.filter(person => person.name.toLowerCase().startsWith(filter))
 
-  return (
-    <div>
-      <h2>Phonebook</h2>
-      <Filter filter={filter} setNewFilter={setNewFilter} />
-      <h3>add a new person</h3>
-      <NewPersonForm addToPhoneBook={addToPhoneBook} newName={newName} newPhoneNumber={newPhoneNumber} setNewName={setNewName} setNewPhoneNumber={setNewPhoneNumber} />
-      <h2>Numbers</h2>
-      <Persons filterPersons={filterPersons} />
-    </div>
-  )
+    console.log(filterPersons)
+    return (
+        <div>
+            <h2>Phonebook</h2>
+            <Filter filter={filter} setNewFilter={setNewFilter} />
+            <h3>add a new person</h3>
+            <NewPersonForm addToPhoneBook={addToPhoneBook} newName={newName} newPhoneNumber={newPhoneNumber} setNewName={setNewName} setNewPhoneNumber={setNewPhoneNumber} />
+            <h2>Numbers</h2>
+            <Persons filterPersons={filterPersons} />
+        </div>
+    )
 }
 
 export default App
