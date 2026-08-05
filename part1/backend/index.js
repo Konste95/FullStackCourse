8b8d46d566f8ab933167eeeac2dcd5f5922cc3da
Konstantin Prokopchik <konstantin.prokopchik@andriyanov.com>
2026-08-05 22:18:09 +0200

    added a log statement about notes

--- part1/backend/index.js
+++ part1/backend/index.js
@@ -79,5 +79,6 @@ app.post('/api/notes', (request, response) => {
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
 
+    console.log(notes)
     console.log(`Server running on port '${PORT}'`)
 })
